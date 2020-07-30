7978c2ec3f0fdbb0d5910274abdbfacf4a9c6983
akshay <akshaymulay27@gmail.com>
2020-07-30 19:26:47 +0530

    Updates

---
+++ precache-manifest.69e21284416aed666f36e52a8bbb205b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a51e73838cba53d73b76b5cc1a2607",
+    "url": "/foodapp/index.html"
+  },
+  {
+    "revision": "437240ec4628fd2bb75e",
+    "url": "/foodapp/static/css/main.15833b0d.chunk.css"
+  },
+  {
+    "revision": "26f5bd02486f1d805677",
+    "url": "/foodapp/static/js/2.d5f18f16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodapp/static/js/2.d5f18f16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "437240ec4628fd2bb75e",
+    "url": "/foodapp/static/js/main.755d8eed.chunk.js"
+  },
+  {
+    "revision": "b8655b62f52c510047a1",
+    "url": "/foodapp/static/js/runtime-main.181afb40.js"
+  },
+  {
+    "revision": "951f9a34e7d785f1be1bb4183cc65e8d",
+    "url": "/foodapp/static/media/final.951f9a34.jpg"
+  }
+]);
